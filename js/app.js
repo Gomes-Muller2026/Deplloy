ba12499d988081ae1a646fa90c376a7db8e1e211
Rafael <rafaelmuller100@gmail.com>
2026-08-06 00:52:35 -0300

    8411

--- js/app.js
+++ js/app.js
@@ -6148,6 +6148,7 @@ class ConsultorioApp {
     document.body.classList.toggle('senha-view', this.currentTab === 'senha');
     document.body.classList.toggle('graficos-view', this.currentTab === 'graficos');
     document.body.classList.toggle('config-view', this.currentTab === 'config');
+    document.body.classList.toggle('panil-view', this.currentTab === 'panil');
 
     if (this.currentTab !== 'agenda') this.agendaReturnTab = null;
     const btnAgendaReturn = document.getElementById('btn-agenda-return-clientes');
